dc8fd7fc7b180b1905af4fd7db1bc3f119f44016
simone andreozzi <simoneandreozzi8@gmail.com>
2022-03-16 16:48:08 +0100

    esercizio-complex

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const app = new Vue(                    // una nuoova instanza di un oggetto di classe vue con varie caratteristiche inserite al suo interno
+    {
+        el: "#app",                         // l'id di partenza di vue e selezionato nel dom html
+
+        data:{
+            message: "" 
+        }
+
+    }
+)
